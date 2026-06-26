a6414f22efd563cc9871b479b55999cb579061e5
buildwithtushar <tushar.sinha.build@gmail.com>
2026-06-26 20:20:20 +0530

    repository for db interaction created

--- src/main/java/com/platformcommons/sms/repository/StudentRepository.java
+++ src/main/java/com/platformcommons/sms/repository/StudentRepository.java
@@ -1,7 +1,27 @@
 package com.platformcommons.sms.repository;
 
 import com.platformcommons.sms.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    boolean existsByStudentCode(String studentCode);
+
+    Page<Student> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    @Query("""
+            SELECT s FROM Student s
+            JOIN s.studentCourses sc
+            WHERE sc.course.id = :courseId
+            """)
+    Page<Student> findStudentsByCourseId(@Param("courseId") Long courseId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"studentCourses", "studentCourses.course"})
+    Optional<Student> findById(Long studentId);
 }
