6cb86bf1093ca055c09c8fe0ff5223ccc7119e8e
buildwithtushar <tushar.sinha.build@gmail.com>
2026-06-26 20:22:09 +0530

    CourseRepository created

--- src/main/java/com/platformcommons/sms/repository/CourseRepository.java
+++ src/main/java/com/platformcommons/sms/repository/CourseRepository.java
@@ -1,7 +1,19 @@
 package com.platformcommons.sms.repository;
 
 import com.platformcommons.sms.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Page<Course> findByCourseNameContainingIgnoreCase(String courseName, Pageable pageable);
+    @Query("""
+            SELECT DISTINCT c FROM Course c
+            JOIN c.topics t
+            WHERE LOWER(t.topicName) LIKE LOWER(CONCAT('%', :topicName, '%'))
+            """)
+    Page<Course> findCoursesByTopicName(@Param("topicName") String topicName, Pageable pageable);
 }
