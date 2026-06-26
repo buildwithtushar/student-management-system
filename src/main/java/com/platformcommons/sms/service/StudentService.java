989330351109387c69d81e9ab932b19255d61f95
buildwithtushar <tushar.sinha.build@gmail.com>
2026-06-26 20:17:42 +0530

    student contract added for implementation

--- src/main/java/com/platformcommons/sms/service/StudentService.java
+++ src/main/java/com/platformcommons/sms/service/StudentService.java
@@ -1,4 +1,23 @@
 package com.platformcommons.sms.service;
 
+import com.platformcommons.sms.dto.*;
+import com.platformcommons.sms.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface StudentService {
+    StudentResponse registerStudent(StudentRequest request);
+    void createCourse(CourseRequest request);
+    void enrollStudentInCourse(EnrollmentRequest request);
+    StudentResponse getStudentProfile(Long studentId);
+
+    StudentResponse updateStudentProfile(Long studentId, StudentUpdateRequest request);
+    void unenrollStudentFromCourse(Long studentId, Long courseId);
+
+    Page<StudentResponse> searchStudentsByName(String name, Pageable pageable);
+    Page<StudentResponse> getStudentsByCourse(Long courseId, Pageable pageable);
+
+    Page<CourseResponse> searchCoursesByName(String courseName, Pageable pageable);
+    Page<CourseResponse> searchCoursesByTopic(String topicName, Pageable pageable);
+
 }
