91a8ea3bc422e6f8b9f5c9663b70338cf1b604e3
buildwithtushar <tushar.sinha.build@gmail.com>
2026-06-26 20:21:20 +0530

    studentCourseRepository created

--- src/main/java/com/platformcommons/sms/repository/StudentCourseRepository.java
+++ src/main/java/com/platformcommons/sms/repository/StudentCourseRepository.java
@@ -3,5 +3,12 @@ package com.platformcommons.sms.repository;
 import com.platformcommons.sms.entity.StudentCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
+
+    List<StudentCourse> findByStudentId(Long studentId);
+    boolean existsByStudentIdAndCourseId(Long studentId, Long courseId);
+    Optional<StudentCourse> findByStudentIdAndCourseId(Long studentId, Long courseId);
 }
